25e9422e5bb5ff3285d6243b41d421ca8b2160c8
Andrei-Diachuck <58138518+Andrei-Diachuck@users.noreply.github.com>
2022-05-27 18:11:05 +0300

    2

--- src/main/java/Projekt/Main.java
+++ src/main/java/Projekt/Main.java
@@ -6,14 +6,11 @@ public class Main {
         Human human = new Human(0);
         Robot robot = new Robot(0);
         Bird bird = new Bird(0);
+        Members members = new Members();
 
-
-        Action[] actions = new Action[6];
-        actions[0] = new Cat(cat.dis(5, 5, 1, 1));
-        actions[1] = new Human(human.dis(10, 1, 15, 1));
-        actions[2] = new Robot(robot.dis(15, 3, 30, 5));
-        actions[3] = new Cat(cat.dis(1, 2, 5, 3));
-        actions[4] = new Human(human.dis(15, 2, 20, 1));
-        actions[5] = new Bird(bird.dis(4, 10, 4, 50));
+        members.getMembers(cat.dis(5, 6, 2, 3));
+        members.getMembers(human.dis(10, 1, 15, 1));
+        members.getMembers(robot.dis(15, 2, 20, 3));
+        members.getMembers(bird.dis(1, 20, 2, 50));
     }
 }
---
+++ src/main/java/Projekt/Members.java
@@ -0,0 +1,10 @@
+package Projekt;
+
+public class Members {
+
+    public Members[] getMembers(int dis) {
+        return members;
+    }
+    Members[] members = new Members[0];
+
+}
